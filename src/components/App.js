38c8c7e1890802028ebfb54f24237a3d38553d2b
luislocon <luis.locon@gmail.com>
2017-08-28 22:05:09 -0600

    Styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import MemeItem from './MemeItem';
 import '../styles/index.css'
 
 class App extends Component {
@@ -19,7 +20,7 @@ class App extends Component {
         {
           this.props.memes.slice(0, this.state.memeLimit).map((meme, index) => {
             return (
-              <h4 key={index}>{meme.name}</h4>
+              <MemeItem key={index} meme={meme} />
             )
           })
         }
